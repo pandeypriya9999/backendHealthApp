b26e55c4c90439acc56bbce1ff9b51d0fc0ed1f6
pandeypriya9999 <pandeypriya9999@gmail.com>
2025-01-27 15:18:05 +0530

    init commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const metricRoutes = require("./routes/metricRoutes");
+const providerRoutes = require("./routes/providerRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/metrics", metricRoutes);
+// app.use("/api/providers", providerRoutes);
+// app.use("/api/messages", messageRoutes);
+
+module.exports = app;
